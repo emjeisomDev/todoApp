94c75f2a48f668e782d7a57627176ff999d8bf19
emjeisomDev <emjeisom.dev@gmail.com>
2023-04-25 21:10:22 -0300

    Applying exceptions. Project finished

--- src/main/java/com/todo/services/ProjectService.java
+++ src/main/java/com/todo/services/ProjectService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.todo.models.Project;
 import com.todo.repositories.ProjectRepository;
+import com.todo.services.exceptions.DatabaseException;
+import com.todo.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ProjectService {
@@ -29,13 +35,24 @@ public class ProjectService {
 	}
 
 	public void delete(Integer id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public Project update(Integer id, Project obj) {
-		Project entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			Project entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			e.printStackTrace();
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(Project entity, Project obj) {
